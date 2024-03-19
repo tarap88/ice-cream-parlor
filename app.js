11e6b5c6edb22df4d7ae2225aa744914b3a0e231
Tara <taraplatt88@gmail.com>
2024-03-19 15:26:09 -0700

    update

--- app.js
+++ app.js
@@ -29,3 +29,47 @@ const toppings = [{
     price: .5,
     quantity: 0
 }]
+
+function drawCart() {
+    let cartContent = ''
+    let cartTotal = 0
+}
+
+
+function orderCookieDough() {
+    console.log('ordering cookieDough')
+
+    let cookieDoughCone = iceCream[0]
+
+    cookieDoughCone.quantity += 1
+    console.log('✨', cookieDoughCone);
+    drawCart()
+
+
+}
+
+
+
+function orderVanilla() {
+    console.log('ordering vanilla')
+
+    let vanillaCone = iceCream[1]
+
+    vanillaCone.quantity += 1
+    console.log('💫', vanillaCone);
+    drawCart()
+
+
+}
+
+function orderStrawberry() {
+    console.log('ordering strawberry')
+
+    let strawberryCone = iceCream[2]
+
+    strawberryCone.quantity += 1
+    console.log('⚡️', strawberryCone);
+    drawCart()
+
+
+}
